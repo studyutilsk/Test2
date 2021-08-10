c3b3707c07dd413ae8d928971d9f66f52e8354e9
Khdeval <65687127+Khdeval@users.noreply.github.com>
2021-08-09 22:51:06 -0700

    Thisis my first commit

---
+++ org.automation.Testngarche/src/test/java/org/automation/Testngarche/org/automation/Testngarche/pages/GoogleSearchResultsPage.java
@@ -0,0 +1,11 @@
+package org.automation.Testngarche.org.automation.Testngarche.pages;
+
+import com.github.jsdevel.testng.selenium.AbstractPage;
+import java.net.URL;
+
+public class GoogleSearchResultsPage extends AbstractPage<GoogleSearchResultsPage, SamplePageFactory> {
+  @Override
+  public boolean isPageViewableFrom(URL proposedUrl) {
+    return "/search".equals(proposedUrl.getPath());
+  }
+}
